6798c8e4472f95775d04045f6b2eae076d7f8647
davidemarcoli <davide@marcoli.ch>
2023-11-18 21:00:32 +0100

    Cleanup

--- index.js
+++ index.js
@@ -23,67 +23,43 @@ function indexToGermanWeekday(weekdayIndex) {
   }
 }
 
-function extractHTPMenus(text, weekdayIndex) {
-  const weekdayName = indexToGermanWeekday(+weekdayIndex)
-  const dayRegex = /(Montag|Dienstag|Mittwoch|Donnerstag|Freitag|Samstag|Sonntag)\s*\d{1,2}\.\s*[A-Za-z]+\s*([\s\S]*?)(?=(Montag|Dienstag|Mittwoch|Donnerstag|Freitag|Samstag|Sonntag|$))/g;
-  let match;
-  let menus = [];
+function extractMenu(text, weekdayIndex, menuCategories) {
+    const weekdayName = indexToGermanWeekday(+weekdayIndex);
+    const dayRegex = /(Montag|Dienstag|Mittwoch|Donnerstag|Freitag|Samstag|Sonntag)\s*\d{1,2}\.\s*[A-Za-z]+\s*([\s\S]*?)(?=(Montag|Dienstag|Mittwoch|Donnerstag|Freitag|Samstag|Sonntag|$))/g;
+    let match;
+    let menus = [];
 
-  while ((match = dayRegex.exec(text)) !== null) {
-      const dayMenu = match[2];
-      const items = splitByPrice(dayMenu);
+    while ((match = dayRegex.exec(text)) !== null) {
+        const dayMenu = match[2];
+        const items = splitByPrice(dayMenu);
 
-      // Check if third menu is a single word (Buffet)
-      if (items[2] && items[2].split(' ').length === 1) {
-          items.splice(2, 0, null); // Insert null for Globetrotter
-      }
+        // Check for special case like 'Buffet' being a single word
+        if (menuCategories.includes('Buffet') && items[2] && items[2].split(' ').length === 1) {
+            items.splice(2, 0, null); // Insert null for Buffet
+        }
 
-      menus.push({
-          day: match[1],
-          Local: cleanMenu(items[0]),
-          Vegi: cleanMenu(items[1]),
-          Globetrotter: cleanMenu(items[2]),
-          Buffet: cleanMenu(items[3])
-      });
-  }
+        let menu = { day: match[1] };
+        menuCategories.forEach((category, index) => {
+            menu[category] = cleanMenu(items[index]);
+        });
 
-  if (weekdayName && weekdayIndex >= 0) {
-    menus = menus.filter(menu => menu.day == weekdayName)[0]
-  }
+        menus.push(menu);
+    }
 
-  return menus;
+    if (weekdayName && weekdayIndex >= 0) {
+        menus = menus.filter(menu => menu.day === weekdayName)[0];
+    }
+
+    return menus;
 }
 
-function extractHT201Menus(text, weekdayIndex) {
-  const weekdayName = indexToGermanWeekday(+weekdayIndex)
-  const dayRegex = /(Montag|Dienstag|Mittwoch|Donnerstag|Freitag|Samstag|Sonntag)\s*\d{1,2}\.\s*[A-Za-z]+\s*([\s\S]*?)(?=(Montag|Dienstag|Mittwoch|Donnerstag|Freitag|Samstag|Sonntag|$))/g;
-  let match;
-  let menus = [];
+// Example usage for HTP menu
+const htpMenuCategories = ['Local', 'Vegi', 'Globetrotter', 'Buffet'];
+// Use like this: extractMenu(pdfText, weekdayIndex, htpMenuCategories)
 
-  while ((match = dayRegex.exec(text)) !== null) {
-      const dayMenu = match[2];
-      const items = splitByPrice(dayMenu);
-
-      // Check if third menu is a single word (Buffet)
-      if (items[2] && items[2].split(' ').length === 1) {
-          items.splice(2, 0, null); // Insert null for Globetrotter
-      }
-
-      menus.push({
-          day: match[1],
-          Local: cleanMenu(items[0]),
-          Global: cleanMenu(items[1]),
-          Vegi: cleanMenu(items[2]),
-          "Pizza & Pasta": cleanMenu(items[3])
-      });
-  }
-
-  if (weekdayName && weekdayIndex >= 0) {
-    menus = menus.filter(menu => menu.day == weekdayName)[0]
-  }
-
-  return menus;
-}
+// Example usage for HT201 menu
+const ht201MenuCategories = ['Local', 'Global', 'Vegi', 'Pizza & Pasta'];
+// Use like this: extractMenu(pdfText, weekdayIndex, ht201MenuCategories)
 
 function splitByPrice(menu) {
   const priceRegex = /Intern\s*\d+\.\d+\s*\/\s*Extern\s*\d+\.\d+/g;
@@ -110,61 +86,41 @@ const port = process.env.port || 3000;
 //const credentials = {key: privateKey, cert: certificate};
 //const httpsServer = https.createServer(credentials, app);
 
-app.get('/htp/:weekdayIndex', (req, res) => {
-  console.log("Weekday", req.params.weekdayIndex)
+function extractMenus(restaurant, text, weekdayIndex) {
+    // Determine which restaurant's menu to extract
+    switch (restaurant) {
+        case 'htp':
+            return extractMenu(text, weekdayIndex, htpMenuCategories);
+        case 'ht201':
+            return extractMenu(text, weekdayIndex, ht201MenuCategories);
+        default:
+            throw new Error('Invalid restaurant');
+    }
+}
 
-  https.get("https://www.betriebsrestaurants-migros.ch/media/k5dnh0sd/landingpage_menueplan_htp.pdf", function(pdfRes) {
-    var data = [];
+// Update route handlers to use the new function
+app.get('/:restaurant/:weekdayIndex', (req, res) => {
+    const { restaurant, weekdayIndex } = req.params;
+    console.log("Restaurant:", restaurant, "Weekday:", weekdayIndex);
 
-    pdfRes.on('data', function(chunk) {
-        data.push(chunk);
-    }).on('end', function() {
-        //at this point data is an array of Buffers
-        //so Buffer.concat() can make us a new Buffer
-        //of all of them together
-        var buffer = Buffer.concat(data);
-        pdf(buffer).then(function(data) {
- 
-          // number of pages
-          // console.log(data.numpages);
-          // number of rendered pages
-          // console.log(data.numrender);
-          // PDF info
-          // console.log(data.info);
-          // PDF metadata
-          // console.log(data.metadata); 
-          // PDF.js version
-          // check https://mozilla.github.io/pdf.js/getting_started/
-          // console.log(data.version);
-          // PDF text
-          // console.log(data.text);   
+    // Determine the URL based on the restaurant
+    const pdfUrl = restaurant === 'htp'
+        ? "https://www.betriebsrestaurants-migros.ch/media/k5dnh0sd/landingpage_menueplan_htp.pdf"
+        : "https://www.betriebsrestaurants-migros.ch/media/x4vjg4pd/menueplan_six-ht201.pdf";
 
-          //console.log(extractMenus(data.text))
-          res.send(extractHTPMenus(data.text, req.params.weekdayIndex))
-      });
-        //console.log(buffer.toString('base64'));
+    https.get(pdfUrl, function(pdfRes) {
+        const data = [];
+
+        pdfRes.on('data', function(chunk) {
+            data.push(chunk);
+        }).on('end', function() {
+            const buffer = Buffer.concat(data);
+            pdf(buffer).then(function(data) {
+                res.send(extractMenus(restaurant, data.text, weekdayIndex));
+            });
+        });
     });
-})});
-
-app.get('/ht201/:weekdayIndex', (req, res) => {
-  console.log("Weekday", req.params.weekdayIndex)
-
-  https.get("https://www.betriebsrestaurants-migros.ch/media/x4vjg4pd/menueplan_six-ht201.pdf", function(pdfRes) {
-    var data = [];
-
-    pdfRes.on('data', function(chunk) {
-        data.push(chunk);
-    }).on('end', function() {
-        //at this point data is an array of Buffers
-        //so Buffer.concat() can make us a new Buffer
-        //of all of them together
-        var buffer = Buffer.concat(data);
-        pdf(buffer).then(function(data) {
-          //console.log(extractMenus(data.text))
-          res.send(extractHT201Menus(data.text, req.params.weekdayIndex))
-      });
-    });
-})});
+});
 
 /*httpsServer*/app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
